086a3af90fb8ec6cfbb84c0b9ead2eb1ed76cd71
tomwilburwright-ux <tomwilburwright@gmail.com>
2026-05-04 09:39:29 +0100

    Update page.tsx

--- examples/with-nextjs/src/app/page.tsx
+++ examples/with-nextjs/src/app/page.tsx
@@ -1,27 +1,27 @@
+"use client";
+
 import dynamic from "next/dynamic";
-import Script from "next/script";
+import "@excalidraw/excalidraw/index.css";
 
-import "../common.scss";
-
-// Since client components get prerenderd on server as well hence importing the excalidraw stuff dynamically
-// with ssr false
-const ExcalidrawWithClientOnly = dynamic(
-  async () => (await import("../excalidrawWrapper")).default,
-  {
-    ssr: false,
-  },
+const Excalidraw = dynamic(
+  () => import("@excalidraw/excalidraw").then((mod) => mod.Excalidraw),
+  { ssr: false }
 );
 
 export default function Page() {
   return (
-    <>
-      <a href="/excalidraw-in-pages">Switch to Pages router</a>
-      <h1 className="page-title">App Router</h1>
-      <Script id="load-env-variables" strategy="beforeInteractive">
-        {`window["EXCALIDRAW_ASSET_PATH"] = window.origin;`}
-      </Script>
-      {/* @ts-expect-error - https://github.com/vercel/next.js/issues/42292 */}
-      <ExcalidrawWithClientOnly />
-    </>
+    <div style={{ height: "100vh" }}>
+      <Excalidraw
+        UIOptions={{
+          canvasActions: {
+            loadScene: false,
+            saveToActiveFile: false,
+            export: false,
+            toggleTheme: false,
+          },
+        }}
+        renderTopRightUI={() => null}
+      />
+    </div>
   );
 }
